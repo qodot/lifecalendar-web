763d164e1d58af136082fe5b1a1316eaa04af931
qodot <waitingforqodot@gmail.com>
2023-11-07 19:32:11 +0900

    RecoilRoot 적용
    layout.tsx가 client component일 수 없으므로 중간 client component 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import RecoilRootProvider from "./RecoilRootProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body className={`${inter.className} prose max-w-none`}>{children}</body>
+      <body className={`${inter.className} prose max-w-none`}>
+        <RecoilRootProvider>{children}</RecoilRootProvider>
+      </body>
     </html>
   );
 }
