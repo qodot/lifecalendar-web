a7d99f8b8cfe0f0b69ab02ee0458861c79b43997
qodot <waitingforqodot@gmail.com>
2023-11-08 18:23:19 +0900

    인증 토큰을 로컬 스토리지에 저장

--- src/recoil/auth.ts
+++ src/recoil/auth.ts
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const accessTokenState = atom<string | null>({
   key: "auth.accessTokenState",
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
