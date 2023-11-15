32248c52b0cb5d4d08e9356d1921194e64471ce3
qodot <waitingforqodot@gmail.com>
2023-11-15 18:06:36 +0900

    재귀 객체에 적용

--- src/util/object.ts
+++ src/util/object.ts
@@ -1,11 +1,27 @@
 import { convertSnakeToCamel } from "./string";
 
+const objectConstructor = {}.constructor;
+const arrayConstructor = [].constructor;
+
+export function isObject(obj: any): obj is Object {
+  return obj && obj.constructor === objectConstructor;
+}
+
+export function isArray(obj: any): obj is any[] {
+  return obj && obj.constructor === arrayConstructor;
+}
+
 export function convertKeysSnakeToCamel(originObject: any): any {
   const convertedObject = {};
 
   for (const [key, value] of Object.entries(originObject)) {
     const camelCaseKey = convertSnakeToCamel(key);
-    convertedObject[camelCaseKey] = value;
+
+    convertedObject[camelCaseKey] = isArray(value)
+      ? value.map((v) => convertKeysSnakeToCamel(v))
+      : isObject(value)
+      ? convertKeysSnakeToCamel(value)
+      : value;
   }
 
   return convertedObject;
